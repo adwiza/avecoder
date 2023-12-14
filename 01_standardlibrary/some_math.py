e3bdef66d3d85457a0ce7b6fcf7cac2a5ccffdba
Acid Wizard <adwiz@suroviy.ru>
2023-12-14 10:13:54 +0300

    2 00:00:00

---
+++ 01_standardlibrary/some_math.py
@@ -0,0 +1,16 @@
+import math
+
+print(math.pi)
+print(math.e)
+
+print(math.nan)
+print(math.inf)
+print(-math.inf)
+
+direction = math.cos(math.pi / 4)
+print(direction)
+print(math.sin(math.pi / 4))
+
+man = 2.5
+print(math.ceil(man))
+print(math.floor(man))
